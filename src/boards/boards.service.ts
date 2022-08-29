737a5d6ac52af7bdd9b19e06d4f676864efa1aab
Jinwon-Dev <yjw9424@naver.com>
2022-08-29 15:50:36 +0900

    feat: uuid를 이용해 게시물을 생성하는 기능 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -8,5 +9,17 @@ export class BoardsService {
   getAllBoards(): Board[] {
     // 타입 정의
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
