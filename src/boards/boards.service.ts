35eddbf65738f56a5accde8d8d41f87b82cff5db
Jinwon-Dev <yjw9424@naver.com>
2022-08-30 11:03:33 +0900

    feat: 에러 메시지 기능 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -29,12 +29,19 @@ export class BoardsService {
 
   getBoardById(id: string): Board {
     // id로 게시물 찾기
-    return this.boards.find((board) => board.id === id); // 특정 게시물 정보 return
+    const found = this.boards.find((board) => board.id === id); // 특정 게시물 정보 return
+
+    if (!found) {
+      // 게시물을 찾는데 없을 경우 에러 메시지
+      throw new NotFoundException(`Can't fine Board with id ${id}`);
+    }
+    return found;
   }
 
   deleteBoard(id: string): void {
     // 게시물 지우기 기능
-    this.boards = this.boards.filter((board) => board.id !== id);
+    const found = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== found.id); // 없는 게시물을 지우려 할 때 에러 메시지
   }
 
   updateBoardStatus(id: string, status: BoardStatus): Board {
